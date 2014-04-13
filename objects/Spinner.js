0ca780a3c746a495fda1f45e6e6046eb940b9cdf
Benedikt Kastl <kontakt@benediktkastl.de>
2014-04-13 19:21:17 +0200

    SVG Spinner (not yet animated)

--- objects/Spinner.js
+++ objects/Spinner.js
@@ -4,7 +4,7 @@ function Spinner(type, count, i) {
 	endColor = getRandomColorRGB(), uid = generateUID(),
 	steps = Math.round(Math.random()*15)+5, runnerIndex = 0, offsetIndex = 0,
 	durationStep = Math.round(Math.random()*2)+5, currentStep = 0,
-	startR = 255, startG = 255, startB = 255, destR = endColor[0], destG = endColor[1], destB = endColor[2];
+	startR = 250, startG = 250, startB = 250, destR = endColor[0], destG = endColor[1], destB = endColor[2];
 	
 	var elementsPerRow = Math.floor(stageWidth / (size*2));
 	var row = Math.floor(i / elementsPerRow);
@@ -22,43 +22,72 @@ function Spinner(type, count, i) {
 			var ctx = canvas.getContext("2d");
 			ctx.translate(size / 2, size / 2);
 		break;
-		case "svg":
-
+		case "svgmulti":
+			var svg = document.createElementNS(svg_ns, "svg");
+			svg.id = uid;
+			svg.setAttribute("width", size);
+			svg.setAttribute("height", size);
+			stage.appendChild(svg);
+			var svgGroup = document.createElementNS(svg_ns, "g");
+			svg.appendChild(svgGroup);
+			paintSpinnerFrame();
 		break;
 	}
 	
+
+
 	function paintSpinnerFrame() {
 		var fillColR,
 	        fillColG,
 	        fillColB,
 	        rotAngle;
 	       var paintIndex = 0;
-	       ctx.clearRect( - size / 2, -size / 2, size, size);
+	      
+	       if (type === "canvasmulti") {
+	       		ctx.clearRect( - size / 2, -size / 2, size, size);
+	   		}
 	       offsetIndex++;
 	       
 	       while(paintIndex < steps)
 			{
-
-		        ctx.save();
-		        fillColR = Math.floor((destR - startR) / steps * paintIndex) + startR;
+				fillColR = Math.floor((destR - startR) / steps * paintIndex) + startR;
 		        fillColG = Math.floor((destG - startG) / steps * paintIndex) + startG;
 		        fillColB = Math.floor((destB - startB) / steps * paintIndex) + startB;
+				switch (type) {
+					case "svgmulti":
+					var domNode = document.createElementNS(svg_ns, "line");
+					svgGroup.appendChild(domNode);
+					domNode.setAttribute("stroke", 'rgb('+fillColR+','+fillColG+','+fillColB+')');
+					domNode.setAttribute("stroke-width", Math.floor(size / 8));
+					domNode.setAttribute("stroke-linecap","round");
+					domNode.setAttribute('x1',Math.floor(size / 3));
+					domNode.setAttribute('y1',size / 2);
+					domNode.setAttribute('x2',Math.floor(size / 8));
+					domNode.setAttribute('y2',size / 2);
+					domNode.setAttribute('transform','rotate('+(360 / steps * paintIndex)+', '+(size/2)+', '+(size/2)+')');
+					break;
+					case "canvasmulti":
+					ctx.save();
+		        
 
-		        ctx.strokeStyle = 'rgb(' + fillColR + ', ' + fillColG + ', ' + fillColB + ')';
-		        ctx.lineWidth = Math.floor(size / 8);
-		        ctx.lineCap = "round";
+			        ctx.strokeStyle = 'rgb(' + fillColR + ', ' + fillColG + ', ' + fillColB + ')';
+			        ctx.lineWidth = Math.floor(size / 8);
+			        ctx.lineCap = "round";
 
-		        ctx.beginPath();
-		        rotAngle = to_rad(360 / steps * paintIndex) + to_rad(360 / steps * offsetIndex);
-		        ctx.rotate(rotAngle);
+			        ctx.beginPath();
+			        rotAngle = to_rad(360 / steps * paintIndex) + to_rad(360 / steps * offsetIndex);
+			        ctx.rotate(rotAngle);
 
-		        ctx.moveTo(0, -Math.floor(size / 8));
-		        ctx.lineTo(0, -Math.floor(size / 3));
+			        ctx.moveTo(0, -Math.floor(size / 8));
+			        ctx.lineTo(0, -Math.floor(size / 3));
 
 
-		        ctx.stroke();
+			        ctx.stroke();
 
-		        ctx.restore();
+			        ctx.restore();
+					break;
+				}
+
 		        ++paintIndex;
 	        }
 	}
@@ -69,11 +98,12 @@ function Spinner(type, count, i) {
 
 		
 		switch (type) {
-         	case 'svg':
+         	case 'svgmulti':
 
         	break;
 
         	case 'canvasmulti':
+        	case 'svgmulti':
         	
         	if (currentStep === durationStep) {
         		currentStep = 0;
